44b4728ac02ac74b6c8717f499daaed1ee5f249f
Agnieszka <azierke90@gmail.com>
2022-03-19 12:48:57 +0100

    add exception package

---
+++ service/src/main/java/zierke/agnieszka/service/exception/CarsServiceException.java
@@ -0,0 +1,7 @@
+package zierke.agnieszka.service.exception;
+
+public class CarsServiceException extends RuntimeException{
+    public CarsServiceException(String message) {
+        super(message);
+    }
+}
